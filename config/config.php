9d62b12a33f56babc154fae617cc841e1b94ae70
Krishna Rimal <genuinetechnology9@gmail.com>
2023-06-30 21:34:50 +0545

    init

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+//DB Params
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "talkingspace");
+
+
+define("SITE_TITLE", "Online Auction System");
+
+//paths
+define('BASE_URI', 'http://' . $_SERVER['SERVER_NAME'] . '/auction/');
